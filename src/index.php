e04261ed224918add823d9aaaf5b9e3987b4f331
Magic <aditjain13@gmail.com>
2012-11-07 23:46:55 +0800

    Wysiwyg Editor Probably has mistakes

--- src/index.php
+++ src/index.php
@@ -15,6 +15,54 @@
 	  jQuery('#VerticalSlidingMenu .SlidingMenu').slidingMenu({ initialOpacity: 0.5 });
 	});
 	</script>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<!-- TinyMCE -->
+	<script type="text/javascript" src="javascripts/tiny_mce/tiny_mce.js"></script>
+	<script type="text/javascript">
+	tinyMCE.init({
+		// General options
+		mode : "textareas",
+		theme : "advanced",
+		plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave,visualblocks",
+
+		// Theme options
+		theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+		theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+		theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+		theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft,visualblocks",
+		theme_advanced_toolbar_location : "top",
+		theme_advanced_toolbar_align : "left",
+		theme_advanced_statusbar_location : "bottom",
+		theme_advanced_resizing : true,
+
+		// Example content CSS (should be your site CSS)
+		content_css : "css/content.css",
+
+		// Drop lists for link/image/media/template dialogs
+		template_external_list_url : "lists/template_list.js",
+		external_link_list_url : "lists/link_list.js",
+		external_image_list_url : "lists/image_list.js",
+		media_external_list_url : "lists/media_list.js",
+
+		// Style formats
+		style_formats : [
+			{title : 'Bold text', inline : 'b'},
+			{title : 'Red text', inline : 'span', styles : {color : '#ff0000'}},
+			{title : 'Red header', block : 'h1', styles : {color : '#ff0000'}},
+			{title : 'Example 1', inline : 'span', classes : 'example1'},
+			{title : 'Example 2', inline : 'span', classes : 'example2'},
+			{title : 'Table styles'},
+			{title : 'Table row 1', selector : 'tr', classes : 'tablerow1'}
+		],
+
+		// Replace values for the template plugin
+		template_replace_values : {
+			username : "Some User",
+			staffid : "991234"
+		}
+	});
+	</script>
+<!-- /TinyMCE -->
 </head>
 <body>
 	<center>
@@ -89,7 +137,25 @@
 			<option name='AD'>AD</option>
 		</select><br>
 		*Invention: <br><input type='text' name='invention'><br>
-		Description: <br><textarea name='desc' id='new-body' class='minLength maxLength ta reg' validatorprops='{minLength:2,maxLength:4999}'></textarea><br>
+		Description: <br>
+		<div>
+			<div>
+			<textarea id='elm1' name='desc' rows='15' cols='80' style='width: 80%'>
+			</textarea>
+			</div>
+			<a href='javascript:;' onclick='tinyMCE.get('elm1').show();return false;'>[Show]</a>
+			<a href='javascript:;' onclick='tinyMCE.get('elm1').hide();return false;'>[Hide]</a>
+			<a href='javascript:;' onclick='tinyMCE.get('elm1').execCommand('Bold');return false;'>[Bold]</a>
+			<a href='javascript:;' onclick='alert(tinyMCE.get('elm1').getContent());return false;'>[Get contents]</a>
+			<a href='javascript:;' onclick='alert(tinyMCE.get('elm1').selection.getContent());return false;'>[Get selected HTML]</a>
+			<a href='javascript:;' onclick='alert(tinyMCE.get('elm1').selection.getContent({format : 'text'}));return false;'>[Get selected text]</a>
+			<a href='javascript:;' onclick='alert(tinyMCE.get('elm1').selection.getNode().nodeName);return false;'>[Get selected element]</a>
+			<a href='javascript:;' onclick='tinyMCE.execCommand('mceInsertContent',false,'<b>Hello world!!</b>');return false;'>[Insert HTML]</a>
+			<a href='javascript:;' onclick='tinyMCE.execCommand('mceReplaceContent',false,'<b>{$selection}</b>');return false;'>[Replace selection]</a>
+			<br />
+			<input type='submit' name='save' value='Submit' />
+			<input type='reset' name='reset' value='Reset' />
+		</div>
 		<input type='submit' name='submit'><br>
 		</form>";
 		$year = mysql_real_escape_string(@$_POST['year']);
@@ -120,7 +186,7 @@
 					}
 				}
 				else 
-				echo "Missing Field, Please Include The Year & Invention Title.
+				echo "Missing Field, Please Include The Year & Invention Title.";
 			}
 		}
 		
@@ -173,5 +239,10 @@
 		</div>
 	</div>
 	</center>
+	<script type="text/javascript">
+	if (document.location.protocol == 'file:') {
+		alert("The examples might not work properly on the local file system due to security settings in your browser. Please use a real webserver.");
+	}
+	</script>
 </body>
 </html>
